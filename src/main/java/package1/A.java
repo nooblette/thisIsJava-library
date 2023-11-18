a63d3c16ddcd90fa9497740d6817c61123b03e24
nooblette <min9hyuk@gmail.com>
2023-11-18 12:37:26 +0900

    feat : ch10 - 라이브러리와 모듈(10.1. 라이브러리)

---
+++ src/main/java/package1/A.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class A {
+	public void method(){
+		System.out.println("package1 - class A method 호출");
+	}
+}
---
+++ src/main/java/package2/B.java
@@ -0,0 +1,7 @@
+package package2;
+
+public class B {
+	public void method(){
+		System.out.println("package1 - class B method 호출");
+	}
+}
